e755cfd02e36268b589246464310e109ce33fc06
faraqhan <f.faraqhan@gmail.com>
2018-09-27 21:26:58 +0300

    add test login user

--- tests/test_account.py
+++ tests/test_account.py
@@ -7,6 +7,7 @@ class TestAccount(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
+    # Tests for create user
     def test_create_user(self):
         post_signup = dict(name="inception", username="Fahad", password="clooneyboss")
         response = self.app.post('/auth/signup', json=post_signup)
@@ -18,7 +19,12 @@ class TestAccount(unittest.TestCase):
         self.assertIn("Please fill missing fields", str(response.data))
 
     def test_create_user_incorrect_input(self):
-        post_signup = dict(name="inception", username= 7, password="clooneyboss")
+        post_signup = dict(name="inception", username=7, password="clooneyboss")
         response = self.app.post('/auth/signup', json=post_signup)
         self.assertIn("Please input correct information", str(response.data))
 
+    # Tests for login user
+    def test_login_user(self):
+        post_login = dict(username="Inception", password="clooneyboss")
+        response = self.app.post('/auth/login', json=post_login)
+        self.assertIn("Inception you have logged created an account", str(response.data))
