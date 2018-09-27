c4b3f2d16fe02a726cde48c149343a7b91e079c1
faraqhan <f.faraqhan@gmail.com>
2018-09-27 20:43:59 +0300

    add test_create_user method

--- tests/test_account.py
+++ tests/test_account.py
@@ -8,3 +8,8 @@ class TestAccount(unittest.TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
+    def test_create_user(self):
+        post_signup = dict(name="inception", username="Fahad", password="clooneyboss")
+        response = self.app.post('/auth/signup', json=post_signup)
+        self.assertIn("Fahad you have successfully created an account", str(response.data))
+
