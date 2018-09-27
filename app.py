61d689ad6f7631c853a35e478c455ce64b49f947
faraqhan <f.faraqhan@gmail.com>
2018-09-27 20:48:19 +0300

    add create_user method

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
+@app.route('/auth/signup', methods=['POST'])
+def create_user():
+    data = request.json
+    name = data.get('name')
+    username = data.get('username')
+    password = data.get('password')
+    return jsonify({'message': '{} you have successfully created an account'.format(username)})
+
 
 if __name__ == '__main__':
     app.run()
