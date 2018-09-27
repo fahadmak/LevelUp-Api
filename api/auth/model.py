086deac5b24396be8b46ff34e04fc827b278d821
faraqhan <f.faraqhan@gmail.com>
2018-09-28 00:18:05 +0300

    [#160814506] add dummy data

--- api/auth/model.py
+++ api/auth/model.py
@@ -17,7 +17,8 @@ class Account:
         }
         return account
 
-account = Account(1, 'fahad', 'fahad3', 'pass123')
-accounts = [account]
+account1 = Account(1, 'fahad', 'fahad3', 'pass123')
+account2 = Account(2, 'mina', 'minatti', 'pass1233')
+accounts = [account1, account2]
 logged_in_accounts = []
 deleted_accounts = []
