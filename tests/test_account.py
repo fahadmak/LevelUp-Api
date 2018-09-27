c5e8a79d0943bc9ed511a1de36a387e69e3f59fa
faraqhan <f.faraqhan@gmail.com>
2018-09-27 21:14:53 +0300

    add test_create_user_incorrect_input method

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,6 +1,5 @@
 import unittest
 from app import app
-from flask import json
 
 
 class TestAccount(unittest.TestCase):
@@ -12,4 +11,14 @@ class TestAccount(unittest.TestCase):
         post_signup = dict(name="inception", username="Fahad", password="clooneyboss")
         response = self.app.post('/auth/signup', json=post_signup)
         self.assertIn("Fahad you have successfully created an account", str(response.data))
+
+    def test_create_user_empty_fields(self):
+        post_signup = dict(name="inception", password="clooneyboss")
+        response = self.app.post('/auth/signup', json=post_signup)
+        self.assertIn("Please fill missing fields", str(response.data))
+
+    def test_create_user_incorrect_input(self):
+        post_signup = dict(name="inception", username= 7, password="clooneyboss")
+        response = self.app.post('/auth/signup', json=post_signup)
+        self.assertIn("Please input correct information", str(response.data))
 
