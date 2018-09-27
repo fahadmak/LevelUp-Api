9486cf64c18e2eaafd28e85df6a3933408c74572
faraqhan <f.faraqhan@gmail.com>
2018-09-28 00:17:02 +0300

    add logout method [#160814506] Delivers

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def create_user():
             return jsonify({'message': 'Please input correct information'})
         elif not item.isalpha():
             return jsonify({'message': 'Please input correct information'})
-    account_id = max([account.accountid for account in accounts]) + 1 if accounts else 1
+    account_id = max([account.account_id for account in accounts]) + 1 if accounts else 1
     account = Account(account_id, name, username, password)
     accounts.append(account)
     return jsonify({'message': '{} you have successfully created an account'.format(username)})
@@ -34,15 +34,33 @@ def login_user():
     account = search_account_by_username(username)
     if not account:
         return jsonify({'message': 'This account does not exist'})
-    if account.password != password:
-        return jsonify({'message': 'User name and password do not match'})
     for item in [username, password]:
         if not isinstance(item, str):
             return jsonify({'message': 'Please input correct information'})
+    if account.password != password:
+        return jsonify({'message': 'User name and password do not match'})
     if account in logged_in_accounts:
         return jsonify({'message': 'You are already logged in'})
     logged_in_accounts.append(account)
     return jsonify({'message': '{} you have logged in'.format(username)})
+
+
+@app.route('/auth/logout', methods=['POST'])
+def logout_user():
+    data = request.json
+    username = data.get('username')
+    if not username:
+        return jsonify({'message': 'Please fill missing fields'})
+    if not isinstance(username, str):
+        return jsonify({'message': 'Please input correct information'})
+    account = search_account_by_username(username)
+    if not account:
+        return jsonify({'message': 'This account does not exist'})
+    if account not in logged_in_accounts:
+        return jsonify({'message': 'You must be logged in to logged out'})
+    logged_out_username = account.username
+    logged_in_accounts.remove(account)
+    return jsonify({'message': '{} you have logged out, until next time '.format(logged_out_username)})
 
 
 if __name__ == '__main__':
