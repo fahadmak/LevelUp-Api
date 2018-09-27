280d29e52694164bc2c4c17016d505881b2b915c
faraqhan <f.faraqhan@gmail.com>
2018-09-28 01:08:21 +0300

    add delete accoount method

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from api.auth.utils import search_account_by_username
+from api.auth.utils import search_account_by_username, search_account_by_id
 from api.auth.model import accounts, Account, logged_in_accounts
 
 app = Flask(__name__)
@@ -61,6 +61,18 @@ def logout_user():
     logged_out_username = account.username
     logged_in_accounts.remove(account)
     return jsonify({'message': '{} you have logged out, until next time '.format(logged_out_username)})
+
+@app.route('/auth/delete/<int:account_id>', methods=['DELETE'])
+def delete_user(account_id):
+    account = search_account_by_id(account_id)
+    if not account:
+        return jsonify({'message': 'This account does not exist'})
+    if account not in logged_in_accounts:
+        return jsonify({'message': 'You must be logged in to deleted your account'})
+    account_name = account.username
+    logged_in_accounts.remove(account)
+    accounts.remove(account)
+    return jsonify({'message': '{} you have deleted your account, until next time '.format(account_name)})
 
 
 if __name__ == '__main__':
