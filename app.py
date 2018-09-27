9d14ebbc89efc0f0d5220249545918e8628ac396
faraqhan <f.faraqhan@gmail.com>
2018-09-27 23:27:41 +0300

    add log_user method

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-
+from api.auth.utils import search_account_by_username
 from api.auth.model import accounts, Account, logged_in_accounts
 
 app = Flask(__name__)
@@ -22,6 +22,27 @@ def create_user():
     account = Account(account_id, name, username, password)
     accounts.append(account)
     return jsonify({'message': '{} you have successfully created an account'.format(username)})
+
+
+@app.route('/auth/login', methods=['POST'])
+def login_user():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    if not all([username, password]):
+        return jsonify({'message': 'Please fill missing fields'})
+    account = search_account_by_username(username)
+    if not account:
+        return jsonify({'message': 'This account does not exist'})
+    if account.password != password:
+        return jsonify({'message': 'User name and password do not match'})
+    for item in [username, password]:
+        if not isinstance(item, str):
+            return jsonify({'message': 'Please input correct information'})
+    if account in logged_in_accounts:
+        return jsonify({'message': 'You are already logged in'})
+    logged_in_accounts.append(account)
+    return jsonify({'message': '{} you have logged in'.format(username)})
 
 
 if __name__ == '__main__':
