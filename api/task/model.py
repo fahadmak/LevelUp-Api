95ee74c833ad71c108ada3cc2d5c25f8d54f5d43
faraqhan <f.faraqhan@gmail.com>
2018-09-28 15:07:58 +0300

    add dummy data

--- api/task/model.py
+++ api/task/model.py
@@ -1,17 +1,21 @@
 class Task:
-    def __init__(self, task_id, task_name, account_id):
+    def __init__(self, task_id, task_name, account_id, marked=False):
         self.task_id = task_id
         self.task_name = task_name
         self.account_id = account_id
+        self.marked = marked
 
     def to_json(self):
         task = {
             'task_id': self.task_id,
             'name': self.task_name,
-            'account_id': self.account_id
+            'account_id': self.account_id,
+            'Marked': self.marked
         }
         return task
 
-
-tasks = []
+task1 = Task(1, 'fahad', 1)
+task2 = Task(2, 'fahad334', 5)
+task3 = Task(3, 'fahad334', 4)
+tasks = [task1, task2, task3]
 deleted_tasks = []
